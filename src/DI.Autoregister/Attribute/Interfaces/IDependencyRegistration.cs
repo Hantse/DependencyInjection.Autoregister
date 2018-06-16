9a7d1539967fbb178b490e88218eecd32a3eea77
Patrick Grasseels <grasseelsp@hotmail.com>
2018-06-16 18:14:07 +0200

    Clear using

--- src/DI.Autoregister/Attribute/Interfaces/IDependencyRegistration.cs
+++ src/DI.Autoregister/Attribute/Interfaces/IDependencyRegistration.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace DI.Autoregister.Attribute.Interfaces
+﻿namespace DI.Autoregister.Attribute.Interfaces
 {
     interface IDependencyRegistration
     {
--- src/DI.Autoregister/Model/ServiceRegistrationType.cs
+++ src/DI.Autoregister/Model/ServiceRegistrationType.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace DI.Autoregister
+﻿namespace DI.Autoregister
 {
     public enum ServiceRegistrationType
     {
